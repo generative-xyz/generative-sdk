9a0f798a6203e32fbef89c519989fa6053da34be
Genesis Doge <doge666@generative.xyz>
2023-02-23 16:21:50 +0700

    fix: build sdk

--- dist/test/utils.spec.js
+++ dist/test/utils.spec.js
@@ -1,9 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai_1 = require("chai");
-describe("Calculator Tests", () => {
-    it("should return 5 when 2 is added to 3", () => {
-        const result = 5;
-        chai_1.assert.equal(result, 5);
-    });
-});
