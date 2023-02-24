486e82e99f7b3bd9ea09d003be4c4d1e433b759f
SangNX <sang@incognito.org>
2023-02-24 12:30:54 +0700

    fix: build alias

--- src/bitcoin/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "@src/bitcoin/tx";
-export * from "@src/bitcoin/utils";
-export * from "@src/bitcoin/types";
-export * from "@src/bitcoin/constants";
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export * as bitcoin from "@src/bitcoin";
+import {broadcastTx, createTx, selectUTXOs} from "./bitcoin/tx";
+
+import {convertPrivateKey, generateTaprootAddress} from "./bitcoin/utils";
+
+import {UTXO} from "./bitcoin/types";
+
+export {
+    convertPrivateKey,
+    createTx,
+    broadcastTx,
+    UTXO,
+    selectUTXOs,
+    generateTaprootAddress,
+};
