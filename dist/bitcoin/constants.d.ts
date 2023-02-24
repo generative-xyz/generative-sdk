bdb9645b86fc306b2cf5b9d39a6eca44ea73467f
0x2525 <2525@generative.xyz>
2023-02-24 17:50:57 +0700

    feat: pdate max transfer amount

--- dist/bitcoin/constants.d.ts
+++ dist/bitcoin/constants.d.ts
@@ -1,3 +1,3 @@
 declare const BlockStreamURL = "https://blockstream.info/api";
-declare const MinSatInscription = 10;
+declare const MinSatInscription = 3000;
 export { BlockStreamURL, MinSatInscription };
--- src/bitcoin/constants.ts
+++ src/bitcoin/constants.ts
@@ -1,5 +1,5 @@
 const BlockStreamURL = "https://blockstream.info/api";
-const MinSatInscription = 10;   // can reduce it to 1 sat
+const MinSatInscription = 3000;  
 
 export {
     BlockStreamURL,
