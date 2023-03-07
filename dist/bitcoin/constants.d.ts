3fd30d76c8c2d409da7209e75a4c1c517e47a6a8
SangNX <matterhorn@generative.xyz>
2023-03-07 12:09:21 +0700

    fix: add sdk error

--- dist/bitcoin/constants.d.ts
+++
@@ -1,8 +0,0 @@
-import { networks } from "bitcoinjs-lib";
-declare const BlockStreamURL = "https://blockstream.info/api";
-declare const MinSats = 1000;
-declare const network: networks.Network;
-declare const DummyUTXOValue = 1000;
-declare const InputSize = 68;
-declare const OutputSize = 43;
-export { BlockStreamURL, MinSats, network, DummyUTXOValue, InputSize, OutputSize };
--- dist/bitcoin/wallet.d.ts
+++
@@ -1,8 +0,0 @@
-import { Inscription, UTXO } from "./types";
-declare const getBTCBalance: (params: {
-    utxos: UTXO[];
-    inscriptions: {
-        [key: string]: Inscription[];
-    };
-}) => number;
-export { getBTCBalance, };
