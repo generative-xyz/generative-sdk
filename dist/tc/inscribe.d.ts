e87aa9aaba61cb59e0d5bcdc887e24cdb20fe7a0
0x2525 <2525@generative.xyz>
2023-04-03 15:03:27 +0700

    feat: func estimate btc amount to inscribe

--- dist/tc/inscribe.d.ts
+++ dist/tc/inscribe.d.ts
@@ -55,4 +55,17 @@ declare const createInscribeTx: ({ senderPrivateKey, utxos, inscriptions, data, 
     revealTxID: string;
     totalFee: BigNumber;
 };
-export { start_taptree, generateInscribeContent, createRawRevealTx, createInscribeTx };
+/**
+* estimateInscribeFee estimate BTC amount need to inscribe for creating project.
+* NOTE: Currently, the function only supports sending from Taproot address.
+* @param htmlFileSizeByte size of html file from user (in byte)
+* @param feeRatePerByte fee rate per byte (in satoshi)
+* @returns the total BTC fee
+*/
+declare const estimateInscribeFee: ({ htmlFileSizeByte, feeRatePerByte, }: {
+    htmlFileSizeByte: number;
+    feeRatePerByte: number;
+}) => {
+    totalFee: BigNumber;
+};
+export { start_taptree, generateInscribeContent, createRawRevealTx, createInscribeTx, estimateInscribeFee };
